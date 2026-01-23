555a1685d0b2842eeb0dd8e7ebf4c8c69c44903a
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2026-01-22 19:07:32 -0600

    Inicio de LAB2

---
+++ Práctica2/Lab2/Lab2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab2.c
+ *
+ * Created: 1/22/2026 4:12:06 PM
+ * Author : juana
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
