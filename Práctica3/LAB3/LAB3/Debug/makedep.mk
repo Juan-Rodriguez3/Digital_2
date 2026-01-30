a7602c9541f75f51e7e05350e20ea568e88dbd2d
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2026-01-29 19:58:33 -0600

    LAB3
    LA programación del Maestro esta hecha

---
+++ Práctica3/LAB3/LAB3/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.c
+
+SPI_Library\SPI.c
+
+UART_library\UART_Library.c
+
