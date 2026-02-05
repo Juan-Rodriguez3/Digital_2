7a5bb570c8a23c5d52be59b611e68d023f479c58
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2026-02-05 00:08:01 -0600

    Inicio de proyecto 1
    Red de sensores con comunicación I2C. El objetivo es utilizar 2 sensores conectados a dos esclavos, un sensor I2C implementada y comunicar la información de sensores y activar los motores atraves de comunicación I2C. También realizar una interfaz en adafruit con el ESP32

---
+++ Proyecto_red_de_sensores/Proyecto_red_de_sensores/main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto_red_de_sensores.c
+ *
+ * Created: 2/5/2026 12:05:06 AM
+ * Author : juana
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
