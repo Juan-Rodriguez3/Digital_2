a4ac59bbd9d29aa31bbf65f9351e49f8818499d0
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2026-02-05 01:52:30 -0600

    Librerias de I2C terminadas
    Falta de implementacion y pruebas

--- Proyecto_red_de_sensores/Proyecto_red_de_sensores/main.c
+++ Proyecto_red_de_sensores/Proyecto_red_de_sensores/main.c
@@ -1,11 +1,15 @@
 /*
  * Proyecto_red_de_sensores.c
- *
+ * Casa inteligente
  * Created: 2/5/2026 12:05:06 AM
- * Author : juana
+ * Juan Rodriguez - 221593
+ * Gerardo Lopez -
+ *	Digital 2 - Seccion 11
  */ 
 
 #include <avr/io.h>
+#include <stdint.h>
+#include "I2C_Libraries/I2C.h"
 
 
 int main(void)
