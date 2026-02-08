2050b44bb93e6fa3c478120373ea182646a35de6
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2026-02-07 20:07:45 -0600

    Inclusión de librerias de LCD
    Modifique las librerias originales para ajustarlas al PINOUT actual.

---
+++ Proyecto_red_de_sensores/Proyecto_red_de_sensores/LCD_Libraries/LCD8bits.c
@@ -0,0 +1,93 @@
+#include "LCD8bits.h"
+
+//PINOUT
+/*
+	RS-->PB2
+	RW-->GND
+	E--> PB3
+	D0-D7 --> PORD0-PORTD7
+*/
+
+void inicio(char a)
+{
+	PORTB &= ~(1<<PORTB2);  //RS = 0, se le indica que es modo comando
+	dato_a_mostrar(a);
+	PORTB |= (1<<PORTB3);    // E = 1, se le indica que envie los datos
+	_delay_ms(1);
+	PORTB &= ~(1<<PORTB3);   // E = 0,  se le indica que se cierra el envio de datos
+	_delay_ms(1);
+}
+
+void dato_a_mostrar(char a)
+{
+	PORTD = a;
+}
+
+
+void Lcd_Init8bits()
+{
+	PORTB &= ~(1<<PORTB2);   //RS = 0, se le indica que es modo comando
+	PORTB &= ~(1<<PORTB3);   //E = 0
+	_delay_ms(20);     //Peque?os delay que indica el fabricante del LCD
+	inicio(0x30);     //Comando que se repite 3 veces, que indica el fabricante de la LCD
+	_delay_ms(5);
+	inicio(0x30);
+	_delay_ms(5);
+	inicio(0x30);
+	_delay_ms(10);
+
+	inicio(0x38);  //Comando que indica el fabricante del LCD, usando la matriz de 5X8
+	inicio(0x0C);  //Comando que indica el fabricante del LCD, display encendido
+	inicio(0x01);  //Comando que indica el fabricante del LCD, Limpiar LCD
+	inicio(0x06);  //Comando que indica el fabricante del LCD, comenzar a almacenar en DDRAM
+}
+
+void Lcd_Set_Cursor(char a, char b)
+{
+	if(a == 0)
+	inicio(0x80 + b);  //Posicionarse en la linea 1 y se suma la columna
+	
+	else if(a == 1)
+	inicio(0xC0 + b);  //Posicionarse en la linea 2  y se suma la columna
+}
+
+void Lcd_Clear()    //Limpia la pantalla LCD
+{
+	inicio(1);  //Comando dado por el fabricante de la LCD
+}
+
+void Lcd_Write_Char(char data)
+{
+	PORTB |= (1<<PORTB2);        // RS = 1, se le indica que esta en modo caracter
+	dato_a_mostrar(data);             //Escribir el caracter
+	PORTB |= (1<<PORTB3);       // E = 1, enviar el dato
+	_delay_ms(1);
+	PORTB &= ~(1<<PORTB3);    // E = 0
+	_delay_ms(1);
+}
+
+
+void Lcd_Write_String(char *a) //Usando punteros, ya que, estos almacenan por ejemplo *dato = hola\0, al final siempre llevan un caracter nulo
+{
+	int i;
+	for(i=0; a[i]!='\0'; i++)   //Se recorre todo el puntero, hasta que el valor sea nulo
+	Lcd_Write_Char(a[i]);
+}
+
+void Lcd_Shift_Right() //Activa el corrimiento hacia la derecha
+{
+	inicio(0x1C);    //Comando brindado por el fabricante de la LCD
+}
+
+void Lcd_Shift_Left()    //Activa el corrimiento hacia la izquierda
+{
+	inicio(0x18);  //Comando brindado por el fabricante de la LCD
+}
+
+
+
+
+
+
+
+
